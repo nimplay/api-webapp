978c8e3ea4ed50cc556cea3fc38d41e8b2dc9267
shahira-sadat <shahira.sadat1@gmail.com>
2022-07-26 12:31:46 +0430

    Fix linterserror

--- dist/bundle.js
+++ dist/bundle.js
@@ -26,7 +26,7 @@ eval("__webpack_require__.r(__webpack_exports__);\n// extracted by mini-css-extr
   \**********************/
 /***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
 
-eval("__webpack_require__.r(__webpack_exports__);\n/* harmony import */ var _style_scss__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./style.scss */ \"./src/style.scss\");\n\n\n\n\n//# sourceURL=webpack://api-webapp/./src/index.js?");
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony import */ var _style_scss__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./style.scss */ \"./src/style.scss\");\n\n\n\n//# sourceURL=webpack://api-webapp/./src/index.js?");
 
 /***/ })
 
